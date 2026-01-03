586eb6c9286812df11adbf048287675b2cd81a28
bipinnegi <bipinnegi.220112935@gehu.ac.in>
2026-01-03 14:38:52 +0530

    Add Create Project UI with POST integration

--- src/app/core/services/project.ts
+++ src/app/core/services/project.ts
@@ -13,4 +13,8 @@ export class ProjectService {
   getProjects(): Observable<any[]>{
     return this.http.get<any[]>(this.apiUrl);
   }
+
+  createProject(name: string , description: string){
+    return this.http.post(this.apiUrl, {name, description });
+  }
 }
--- src/app/projects/project-list/project-list.ts
+++ src/app/projects/project-list/project-list.ts
@@ -1,20 +1,36 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 import { ProjectService } from '../../core/services/project';
 import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-project-list',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './project-list.html',
   styleUrls: ['./project-list.css']
+  
 })
 export class ProjectListComponent {
 
-  projects$: Observable<any[]>;
+  projects$!: Observable<any[]>;
+  name='';
+  description= '';
 
   constructor(private projectService: ProjectService) {
+    this.loadProjects();
+  }
+
+  loadProjects(){
     this.projects$ = this.projectService.getProjects();
+  }
+
+  createProject(){
+    this.projectService.createProject(this.name, this.description).subscribe(() =>{
+           this.name='';
+           this.description='';
+           this.loadProjects();
+    });
   }
 }
