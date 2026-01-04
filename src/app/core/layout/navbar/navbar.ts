ba6e5b99f2d72f17aeab449caac4d1fb343f4019
bipinnegi <bipinnegi.220112935@gehu.ac.in>
2026-01-04 12:34:33 +0530

    Fix navbar routerLink by importing RouterModule

--- src/app/core/layout/navbar/navbar.ts
+++ src/app/core/layout/navbar/navbar.ts
@@ -1,25 +1,41 @@
-import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../../services/auth';
+import { Router, NavigationEnd, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [CommonModule, RouterModule],
-  templateUrl: './navbar.html',
-  styleUrl: './navbar.css',
-})
-export class NavbarComponent {
+  imports: [RouterModule],
+  templateUrl: './navbar.html'
   
+})
+export class NavbarComponent implements OnInit {
 
   isOwner = false;
+  isLoggedIn = false;
 
-  constructor(private authService: AuthService) {
-  this.isOwner = this.authService.getUserRole() === 'Owner';
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {
+    // Run on every route change
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        this.updateAuthState();
+      }
+    });
   }
 
-  logout(){
+  updateAuthState() {
+    const role = this.authService.getUserRole();
+    this.isLoggedIn = !!role;
+    this.isOwner = role === 'Owner';
+  }
+
+  logout() {
     this.authService.logout();
+    this.updateAuthState();
   }
 }
