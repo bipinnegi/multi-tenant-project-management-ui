4e5c2734842d0a0fa09d7a585b8e6ccc7e34cc85
bipinnegi <bipinnegi.220112935@gehu.ac.in>
2026-01-08 15:00:53 +0530

    fix: add https to azure api base url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiBaseUrl: 'project-manager-api-bipin-dpayepfgeqhug8hy.centralindia-01.azurewebsites.net'
+  apiBaseUrl: 'https://project-manager-api-bipin-dpayepfgeqhug8hy.centralindia-01.azurewebsites.net'
 };
