7d43791a77329ca639e6c5a20d5a2471ca1eaf29
bipinnegi <bipinnegi.220112935@gehu.ac.in>
2026-03-18 21:17:53 +0530

    few changes in environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiBaseUrl: 'https://project-manager-api-bipin-dpayepfgeqhug8hy.centralindia-01.azurewebsites.net'
+  apiBaseUrl: 'https://api.projexflow.me'
 };
